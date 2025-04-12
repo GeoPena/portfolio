872233645525db2da7a086141722290530519397
geopena <gep866@ensign.edu>
2025-04-11 22:18:29 -0600

    Updates

---
+++ _next/static/chunks/app/about/page-bf8b71922a1eab72.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,895],{9570:function(n,t,e){Promise.resolve().then(e.t.bind(e,1749,23))},5600:function(n,t,e){"use strict";function u(n){let{src:t}=n;return"/portfolio".concat(t)}e.r(t),e.d(t,{default:function(){return u}})}},function(n){n.O(0,[749,971,69,744],function(){return n(n.s=9570)}),_N_E=n.O()}]);
